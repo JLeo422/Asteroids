60c2c7701a5819fa80eecb60b6b9940311c6e851
JLeo422 <jason.leonhardt.422@gmail.com.com>
2025-11-25 12:24:36 -0500

    Completed Game

--- main.py
+++ main.py
@@ -1,22 +1,72 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH 
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    
+
+    print(f"Starting Asteroids with pygame version: {pygame.__version__}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        pygame.display.flip()
 
-    print(f"Starting Asteroids with pygame version: {pygame.__version__}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+                    break
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for loop in drawable:
+            loop.draw(screen)
+ 
+
+        pygame.display.flip()
+        dt = clock.tick(60) /1000
+
+
+
 
 
 if __name__ == "__main__":
